863681e1ee469af1ecbd694b918058500b1c7060
Ivanpk1 <ivanpk@umich.edu>
2024-04-07 16:43:57 -0400

    get_players_by_country function completed

--- NBA_API.py
+++ NBA_API.py
@@ -1,25 +1,28 @@
 import requests
 
-url = "https://api-nba-v1.p.rapidapi.com/players"
+def get_nba_players_by_country(country):
+    url = "https://api-nba-v1.p.rapidapi.com/players"
+    querystring = {"country": country}
+    headers = {
+        "X-RapidAPI-Key": "1e690ea0camshae97b1517e56281p1a8b47jsnf31096b1ea77",
+        "X-RapidAPI-Host": "api-nba-v1.p.rapidapi.com"
+    }
 
+    response = requests.get(url, headers=headers, params=querystring)
+    data = response.json()
+
+    if data.get("results", 0) > 0:
+        # gather names and country
+        player_info = [(player["firstname"], player["lastname"], player["birth"]["country"]) for player in data["response"]]
+        count = len(player_info)
+
+        for player in player_info:
+            print(player)
+    else:
+        print("No player was found")
+
+    print(f"{country} has {count} players in the NBA.")
+
+# example
 country = input("Enter country: ")
-
-querystring = {"country": country}
-
-headers = {
-	"X-RapidAPI-Key": "1e690ea0camshae97b1517e56281p1a8b47jsnf31096b1ea77",
-	"X-RapidAPI-Host": "api-nba-v1.p.rapidapi.com"
-}
-
-response = requests.get(url, headers=headers, params=querystring)
-data = response.json()
-# print(data)
-
-if data.get('results', 0) > 0:
-    # gather names and country
-    player_info = [(player['firstname'], player['lastname'], player['birth']['country']) for player in data['response']]
-
-    for player in player_info:
-        print(player)
-else:
-    print("No player was found")
+get_nba_players_by_country(country)
