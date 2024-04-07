99183534d4ee5173328d6aebb2e669a010720ba1
Ivanpk1 <ivanpk@umich.edu>
2024-04-07 16:15:37 -0400

    data acquired

--- NBA_API.py
+++ NBA_API.py
@@ -1,35 +1,25 @@
 import requests
-import json
-import unittest
-import os
 
-API_KEY = "1e690ea0camshae97b1517e56281p1a8b47jsnf31096b1ea77"
+url = "https://api-nba-v1.p.rapidapi.com/players"
 
-def load_json(filename):
-    try:
-        with open(filename, "r") as file:
-            json_data = json.load(file)
-            print(f"FILE {filename} LOADED.")
-            return json_data
-    except FileNotFoundError:
-        print(f"FILE {filename} NOT FOUND.")
-        return {}
+country = input("Enter country: ")
 
-def write_json(dict, filename):
-    try:
-        with open(filename, "w") as file:
-            json.dump(dict, file)
-            print(f"DATA WRITTEN IN {filename}")
-    except Exception as e:
-        print(f"ERROR WRITING {filename}")
+querystring = {"country": country}
 
-def get_nba_data(player):
-    # parameters
-    url = "api-nba-v1.p.rapidapi.com"
-    params = {
-        "apikey": API_KEY,
-        "t": player
-    }
-    # convert response data to json
-    response = requests.get(url, params=params)
+headers = {
+	"X-RapidAPI-Key": "1e690ea0camshae97b1517e56281p1a8b47jsnf31096b1ea77",
+	"X-RapidAPI-Host": "api-nba-v1.p.rapidapi.com"
+}
 
+response = requests.get(url, headers=headers, params=querystring)
+data = response.json()
+# print(data)
+
+if data.get('results', 0) > 0:
+    # gather names and country
+    player_info = [(player['firstname'], player['lastname'], player['birth']['country']) for player in data['response']]
+
+    for player in player_info:
+        print(player)
+else:
+    print("No player was found")
