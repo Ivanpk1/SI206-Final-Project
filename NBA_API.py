2f85418bf2a04aab354ffb026ee49dc454c3ec67
RaYaNmOsToFa <144081133+RaYaNmOsToFa@users.noreply.github.com>
2024-04-30 10:41:00 -0400

    Add files via upload

---
+++ NBA_API.py
@@ -0,0 +1,100 @@
+import requests
+import sqlite3
+
+def create_table():
+    """
+    Create table
+
+    Args: none
+
+    Returns: none
+    """
+    conn = sqlite3.connect("countries.db")
+    curr = conn.cursor()
+    curr.execute('''CREATE TABLE IF NOT EXISTS NBA (
+                        id INTEGER PRIMARY KEY,
+                        firstname TEXT,
+                        lastname TEXT,
+                        country_id INTEGER
+                    )''')
+    conn.commit()
+    conn.close()
+
+def insert_players(player_info):
+    """
+    Insert player info into the table
+
+    Args: player_info (tuple), first name, last name, country
+
+    Returns: none
+    """
+    conn = sqlite3.connect("countries.db")
+    curr = conn.cursor()
+    counter = 0
+    for player in player_info:
+        # print(player)
+        usa = player[0][2]
+        if usa == "USA":
+            usa = "United States"
+        curr.execute("SELECT id FROM Countries WHERE Country = ?", (usa,))
+        country_id = curr.fetchone()
+        # print(country_id)
+        
+        curr.execute("SELECT * FROM NBA WHERE firstname = ?", (player[0][0],))
+        result = curr.fetchone()
+        
+        if result is not None:
+            continue
+        else:
+            if country_id:
+                if counter == 24:
+                    break            
+                curr.execute("INSERT OR IGNORE INTO NBA (firstname, lastname, country_id) VALUES (?, ?, ?)", (player[0][0], player[0][1], country_id[0]))
+                print(player[0][0], player[0][1], country_id[0])
+                counter += 1
+    
+    conn.commit()
+    conn.close()
+
+def get_players():
+    """
+    Retrieve NBA players from a specific country from the API
+
+    Args: country (str), country of origin
+
+    Returns: none
+    """
+    data_list = []
+    for i in range(1,25):
+        i = str(i*25)
+        url = (f"http://api.balldontlie.io/v1/players?cursor={i}&per_page=25")
+        print(url)
+        headers = {
+            "Authorization": "0fab7ce8-dd2f-403b-b6fa-4ab027f7d18e",
+        }
+        try:
+            response = requests.get(url, headers=headers)
+            
+            
+            data = response.json()
+            # print(data)
+
+            # gather names and country
+            for player in data["data"]:
+                player_info= [(player["first_name"], player["last_name"], player["country"])]
+                # print(player_info)
+                if player_info not in data_list:
+                    # print(player_info)
+                    data_list.append(player_info)
+        except:
+            continue
+        # print(len(data_list))
+        
+    return data_list
+
+def main():
+    create_table()
+    players_list = get_players()
+    insert_players(players_list)
+
+main()
